cc19034b92ce9061ba16d073526df2ff71aba62b
Sylvester <sylvester.duda@gmail.com>
2019-05-21 13:11:55 -0700

    Better drectory support  and sample JSON files.

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
+import os
+import json
 import lxml.etree as et
 from collections import OrderedDict
+from utility.string import truncateFilePath
 from utility.jsbron import jsbron
-import json
+
+SUFFIX = "output"
 
 INSTANCE = "data\\fb\\2019\\10-K\\fb-20181231.xml"
 SCHEMA = "data\\fb\\2019\\10-K\\fb-20181231.xsd" #NEED TO IMPORT OTHER
@@ -11,18 +15,21 @@ LABEL = "data\\fb\\2019\\10-K\\fb-20181231_lab.xml"
 PRESENTATION = "data\\fb\\2019\\10-K\\fb-20181231_pre.xml"
 
 jsbron = jsbron()
-jsbron.convert_XML(calculation=CALCULATION, definition=DEFINITION, label=LABEL, presentation=PRESENTATION, delimiter="  ")
+jsbron.convert_XML(instance=INSTANCE, calculation=CALCULATION, definition=DEFINITION, label=LABEL, presentation=PRESENTATION, delimiter="  ")
 
 def writeToJSONFile(path, fileName, data):
-    filePathNameWExt = './' + path + '/' + fileName + '.json'
+    path =   '.\\' + path + '\\'
+    if not os.path.exists(path):
+        os.makedirs(path)
+    filePathNameWExt = path + fileName + '.json'
     with open(filePathNameWExt, 'w') as fp:
         fp.write(json.dumps(data, sort_keys=True, indent=4, separators=(',', ': ')))
 
-#writeToJSONFile('','fb_2019_instance', jsbron.instance)
-writeToJSONFile('','fb_2019_calculation', jsbron.taxonomy_calculation)
-writeToJSONFile('','fb_2019_definition', jsbron.taxonomy_definition)
-writeToJSONFile('','fb_2019_label', jsbron.taxonomy_label)
-writeToJSONFile('','fb_2019_presentation', jsbron.taxonomy_presentation)
-#writeToJSONFile('','fb_2019_schema', jsbron.taxonomy_schema)
+writeToJSONFile(truncateFilePath(INSTANCE, suffix=SUFFIX),'fb_2019_instance', jsbron.instance)
+writeToJSONFile(truncateFilePath(CALCULATION, suffix=SUFFIX),'fb_2019_calculation', jsbron.taxonomy_calculation)
+writeToJSONFile(truncateFilePath(DEFINITION, suffix=SUFFIX),'fb_2019_definition', jsbron.taxonomy_definition)
+writeToJSONFile(truncateFilePath(LABEL, suffix=SUFFIX),'fb_2019_label', jsbron.taxonomy_label)
+writeToJSONFile(truncateFilePath(PRESENTATION, suffix=SUFFIX),'fb_2019_presentation', jsbron.taxonomy_presentation)
+#writeToJSONFile(truncateFilePath(SCHEMA, suffix=SUFFIX),'fb_2019_schema', jsbron.taxonomy_schema)
 
 print("End of File")
---
+++ utility/string.py
@@ -0,0 +1,18 @@
+def truncateFilePath(string, suffix=None, parts=1):
+    """Function that takes a file that was loaded already and truncates it to make a sub folder with a child folder specified.
+
+        Attr:
+            string(str): Original path to be truncated.
+            suffix(str): Altered path inside of truncated path.
+            parts(int): How far you would like to truncate it. Set to 1 by default for directories with a file at the end.
+
+    """
+    string = string.split("\\")
+    string.pop()
+    new_string = ""
+    for s in string:
+        new_string += s + "\\"
+    if suffix:
+        new_string += suffix + "\\"
+    new_string = new_string[:-1]
+    return new_string
